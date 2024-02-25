e3d258df2a9ffdb9691e10f4a378298c2e296021
VSebastian8 <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2024-02-25 22:38:34 +0200

    Keyboard
    Used the Shakespeare template languages: Hamlet, Cassius and Julius to add HTML, CSS and JS

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,11 +3,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes       #-}
 module Handler.Home where
 
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
+-- import Text.Julius (RawJS (..))
 
 -- Define our data that will be used for creating the form.
 data FileForm = FileForm
@@ -22,9 +23,16 @@ data FileForm = FileForm
 -- The majority of the code you will write in Yesod lives in these handler
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
+
+allTiles :: [[Text]]
+allTiles = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"], 
+            ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+            ["ENTER", "Z", "X", "C", "V", "B", "N", "M" ,"BACK"]]
+
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
         aDomId <- newIdent
         setTitle "Wordle"
         $(widgetFile "homepage")
+        $(widgetFile "tiles")
